d612287b9772b9cb5e43d81d0a9c90ed85370e39
gowasu-hydrangea <114853745+gowasu-hydrangea@users.noreply.github.com>
2023-05-10 14:15:15 +0000

    Fix Routing

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'lists' => 'lists#index'
   # get 'lists/show'
   # 基礎知識学習フェーズ/アプリケーションを完成させよう/4章
-  get 'lists/:id' => 'lists#show'
+  get 'lists/:id' => 'lists#show', as: 'list'
   get 'lists/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
