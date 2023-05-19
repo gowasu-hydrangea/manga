1ca5bbc538c81666d6d61890f03e0f96d77fefca
gowasu-hydrangea <114853745+gowasu-hydrangea@users.noreply.github.com>
2023-05-19 07:27:42 +0000

    Fix Routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   
   get 'lists/new'
+  
+  get 'top' => 'homes#top'
+  
   post 'lists' => 'lists#create'
+  
   # get 'lists/index'
   get 'lists' => 'lists#index'
   
@@ -18,6 +22,5 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  get 'top' => 'homes#top'
   
 end
