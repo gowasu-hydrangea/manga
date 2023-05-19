aa981cafa41d957555f7edf7966ac55b4c815ecd
gowasu-hydrangea <114853745+gowasu-hydrangea@users.noreply.github.com>
2023-05-19 07:25:35 +0000

    Fix Lists Controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -29,6 +29,7 @@ class ListsController < ApplicationController
   def update
     list = List.find(params[:id])
     list.update(list_params)
+    redirect_to list_path(list.id)
   end
   
   private
